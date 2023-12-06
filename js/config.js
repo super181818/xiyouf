a6190a651a60548b878139eb3d0bdb8cf17546d3
super181818 <131360198+super181818@users.noreply.github.com>
2023-12-06 15:39:31 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,13 @@
+/*-------config start-------*/
+var _wip = '101.33.243.64';  
+var _port = '1741';   
+/*---------config end---------*/
+
+var BaseUrl = 'http://' + _wip+':'+_port;
+var ResourceUrl = '/';
+var Suffix = '.html';
+var _isTx = 1;
+var _wport = '10001' ;
+
+
+/*document.write('<script type="text/javascript" src="http://' + _wip + ':1999/dyoss.js?v=' + _tt + '"> <\/script>');*/
